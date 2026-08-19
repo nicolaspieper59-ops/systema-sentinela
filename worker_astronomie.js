ba696bcdd9f514330d219ed62ac842bd6ef25eab
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2026-08-19 09:19:26 -0100

    Update worker_astronomie.js

--- worker_astronomie.js
+++ worker_astronomie.js
@@ -1,3 +1,4 @@
+// Importation des modules (fonctionne dans un Web Worker moderne)
 importScripts('vsop2013.js', 'ElpMpp02DE_min.js');
 
 function mod2pi(angle) {
@@ -5,41 +6,46 @@ function mod2pi(angle) {
     let res = angle % twoPi;
     return res < 0 ? res + twoPi : res;
 }
+
 self.CYCLE = mod2pi;
 if (typeof self.mod2pi_DE === 'undefined') self.mod2pi_DE = mod2pi;
 
 const CORPS_CELESTES = ['soleil', 'lune', 'mercure', 'venus', 'mars', 'jupiter', 'saturne', 'uranus', 'neptune'];
 
 self.onmessage = function(event) {
-    const { type, jd, station } = event.data;
+    const data = event.data || {};
+    const type = data.type;
+    const jd = data.jd;
 
-    if (type === 'COMPUTE' || event.data.action === 'CALCULATE') {
+    if (type === 'COMPUTE' || data.action === 'CALCULATE') {
         try {
-            if (typeof jd !== 'number' || isNaN(jd)) throw new Error("JD invalide.");
+            if (typeof jd !== 'number' || isNaN(jd)) {
+                throw new Error("Date Julian (JD) invalide ou manquante.");
+            }
 
             let results = {};
+            // Siècles juliens depuis J2000.0 (utilisé généralement pour la Lune / ELP)
             const T_siecles = (jd - 2451545.0) / 36525.0;
+            // Millénaires juliens depuis J2000.0 (requis par VSOP2013)
             const jy2k = (jd - 2451545.0) / 365250.0;
 
             CORPS_CELESTES.forEach(astre => {
-                let rawCoords;
                 if (astre === 'lune') {
-                    rawCoords = getX2000_DE(T_siecles);
-                } else {
-                    if (typeof vsop2013 !== 'undefined' && vsop2013[astre]) {
-                        rawCoords = vsop2013[astre](jy2k);
+                    if (typeof getX2000_DE === 'function') {
+                        results[astre] = getX2000_DE(T_siecles);
+                    } else if (typeof getX2000_LLR === 'function') {
+                        results[astre] = getX2000_LLR(T_siecles);
                     } else {
-                        // Valeur de secours simulée ou erreur gérée
-                        rawCoords = { elevation: 15.0, azimuth: 45.0, distance: 150000000 };
+                        throw new Error("Fonction de calcul pour la Lune introuvable.");
+                    }
+                } else {
+                    // Pour le Soleil et les planètes via VSOP2013
+                    if (typeof vsop2013 !== 'undefined' && typeof vsop2013[astre] === 'function') {
+                        results[astre] = vsop2013[astre](jy2k);
+                    } else {
+                        throw new Error(`Module VSOP2013 introuvable ou fonction absente pour : ${astre}`);
                     }
                 }
-
-                // Simulation des calculs topocentriques finaux si non présents dans les modules bruts
-                results[astre] = {
-                    elevation: rawCoords.elevation !== undefined ? rawCoords.elevation : (Math.random() * 80 - 10),
-                    azimuth: rawCoords.azimuth !== undefined ? rawCoords.azimuth : (Math.random() * 360),
-                    distance: rawCoords.distance !== undefined ? rawCoords.distance : 150000000
-                };
             });
 
             self.postMessage({
